b4daed98312212e03c2a55ca2780b2159ef56ea2
sivagurujula <sivagurujula@gmail.com>
2020-07-21 20:43:30 +0530

    Updates

---
+++ precache-manifest.d2b721e8051ccc09ca99b984f6a4e362.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64bc9a088fe085a638a3d58132c430ec",
+    "url": "/order-prototype-app/index.html"
+  },
+  {
+    "revision": "7397c0308bd52c4d55be",
+    "url": "/order-prototype-app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "82b1a6272be74e70a57f",
+    "url": "/order-prototype-app/static/css/main.dc764a7e.chunk.css"
+  },
+  {
+    "revision": "7397c0308bd52c4d55be",
+    "url": "/order-prototype-app/static/js/2.b4943a3d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/order-prototype-app/static/js/2.b4943a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82b1a6272be74e70a57f",
+    "url": "/order-prototype-app/static/js/main.6dc22cc1.chunk.js"
+  },
+  {
+    "revision": "bfec578a03c6a1298ac2",
+    "url": "/order-prototype-app/static/js/runtime-main.075b8345.js"
+  },
+  {
+    "revision": "4a7b429e037b5ee95531fc91104145f4",
+    "url": "/order-prototype-app/static/media/appointmentMsg.4a7b429e.jpg"
+  }
+]);
